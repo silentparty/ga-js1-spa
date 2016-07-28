247e8dcefd18231b793fa6465df967fd35d6850e
Marcel Jacobs <silent.party@gmail.com>
2016-07-28 14:10:01 +1000

    :(

---
+++ docs/slides.md
@@ -0,0 +1,81 @@
+# Hello :wave:
+
+---
+
+# Idea 1 - How Are people Feeling in X? :smile:
+
+---
+
+- Hit the Twitter API, and the Google natural language service
+- Run tweets from a region through the Language service
+- Get the sentiment value
+- Boom, you know (roughly) how people are feeling in that area
+- Build frontend that allows people to query the api
+- Profit $$$ etc
+
+---
+
+# Roadblock! :sweat:
+
+- I wound up writing my own API.
+- The assignment isn't about writing APIs
+
+(demo)
+
+---
+
+# Idea 2 - Headspace :smile:
+
+---
+
+It's not easy to quickly find parks / greenspace, open space around sydney
+- Add points to a google map
+- Render the map
+- Clicking pins will load the content into the sidebar
+- clicking sidebar content will focus map
+- etc
+
+(demo)
+
+---
+
+# Roadblock! :sweat:
+Google Maps is a piece of :poop:.
+It's a mess of `new` and `this`. It hurt my brain.
+
+---
+
+# Idea 4 - Chat client! :smile:
+
+---
+
+- With `.on()` I could make a chat client!
+- How to do CRUD easily?
+- This was last night
+
+---
+
+# Idea 4 - Fuck it (aka address book) :poop:
+
+---
+
+It's an address book
+- Pulls in user data from Firebase
+- Renders a static map using the address
+- You can add more entries, delete entries (CRUD basically)
+
+(demo)
+
+---
+
+# Roadblock! :skull:
+- I started it this morning. So uh, time was a problem.
+- I forgot how to code. Seriously.
+
+---
+
+# What I've learned overall
+- Doing really cool stuff with APIs is a lot easier server-side
+- Being realistic is hard
+- Start earlier
+- Don't sweat the small stuff, just move on to another feature
---
+++ public/js/main.js
@@ -0,0 +1,71 @@
+'use strict'
+
+// TODO Render sidebar items from firebase data
+// TODO render detail view when sidebar item clicked
+// TODO clicking 'add' button invokes empty detail view
+// TODO add a 'delete' button to the detail template
+// TODO when a user clicks delete button, the data is deleted from firbase, and the user is left iwht the default view
+
+//
+
+;(function () {
+  var container = document.querySelector('#container')
+  var sidebar = document.querySelector('.demo-list-two')
+
+  var state = {}
+  const config = {
+    apiKey: 'AIzaSyBC9k1udNheyFSYuCEkovjfdXxm-De0ROE',
+    authDomain: 'mythical-zodiac-138523.firebaseapp.com',
+    databaseURL: 'https://mythical-zodiac-138523.firebaseio.com',
+    storageBucket: 'mythical-zodiac-138523.appspot.com'
+  }
+  //
+  const fbRootPath = 'users/'
+
+  // Firebase Database
+  firebase.initializeApp(config)
+
+  // handy firebase reference
+  const db = firebase.database()
+
+  ;(function () {
+    console.log('contacting firebase...')
+    firebase.database().ref().once('value').then(function (snapshot) {
+      return snapshot.val()
+    }).then((snapshot) => {
+      state = snapshot
+      renderUserList(state, sidebar)
+    })
+      .catch((err) => {
+        console.error('there was a problem initialising the database', err)})
+  })()
+
+  delegate('body', 'click', '.mdl-button', function (event) {
+    console.log('you clicked', event.delegateTarget)
+  })
+
+  // Render the user list
+  function renderUserList (data, into) {
+    console.log('rendering userList')
+    console.log(data.users)
+    into.innerHTML = `
+    ${data.users.map((item, index) => {
+      return `
+        <li class="mdl-list__item mdl-list__item--one-line" data->
+        <span class="mdl-list__item-primary-content">
+          <i class="material-icons mdl-list__item-avatar">person</i>
+          <span>${item.name.first} ${item.name.last}</span>
+        </li>
+        `
+    }).join('')}
+    `
+    componentHandler.upgradeDom()
+    console.log('renderUserlist complete', data)
+  }
+
+  render(state, container)
+
+  function render (data, into) {
+    // TODO
+  }
+})()
