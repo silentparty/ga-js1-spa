31e09753ea725314ff4dc43ad399fe121b106920
Marcel Jacobs <silent.party@gmail.com>
2016-09-07 15:40:41 +1000

    adding users, pushing to array

--- public/js/main.js
+++ public/js/main.js
@@ -4,68 +4,107 @@
 // TODO render detail view when sidebar item clicked
 // TODO clicking 'add' button invokes empty detail view
 // TODO add a 'delete' button to the detail template
-// TODO when a user clicks delete button, the data is deleted from firbase, and the user is left iwht the default view
+// TODO when a user clicks delete button, the data is deleted from firbase, and the user is left with the default view
 
+// TODO Re-enable me at the end (private data)
+// ;(function () {
+var container = document.querySelector('#container')
+var sidebar = document.querySelector('.demo-list-two')
+
+var state = {
+  users: []
+}
+const config = {
+  apiKey: 'AIzaSyBC9k1udNheyFSYuCEkovjfdXxm-De0ROE',
+  authDomain: 'mythical-zodiac-138523.firebaseapp.com',
+  databaseURL: 'https://mythical-zodiac-138523.firebaseio.com',
+  storageBucket: 'mythical-zodiac-138523.appspot.com'
+}
 //
+const fbRootPath = 'users/'
+
+// Firebase Database
+firebase.initializeApp(config)
+
+// handy firebase reference
+const db = firebase.database()
+
+/*
+███████ ██ ██████  ███████ ██████   █████  ███████ ███████
+██      ██ ██   ██ ██      ██   ██ ██   ██ ██      ██
+█████   ██ ██████  █████   ██████  ███████ ███████ █████
+██      ██ ██   ██ ██      ██   ██ ██   ██      ██ ██
+██      ██ ██   ██ ███████ ██████  ██   ██ ███████ ███████
+*/
 
 ;(function () {
-  var container = document.querySelector('#container')
-  var sidebar = document.querySelector('.demo-list-two')
-
-  var state = {}
-  const config = {
-    apiKey: 'AIzaSyBC9k1udNheyFSYuCEkovjfdXxm-De0ROE',
-    authDomain: 'mythical-zodiac-138523.firebaseapp.com',
-    databaseURL: 'https://mythical-zodiac-138523.firebaseio.com',
-    storageBucket: 'mythical-zodiac-138523.appspot.com'
-  }
-  //
-  const fbRootPath = 'users/'
-
-  // Firebase Database
-  firebase.initializeApp(config)
-
-  // handy firebase reference
-  const db = firebase.database()
-
-  ;(function () {
-    console.log('contacting firebase...')
-    firebase.database().ref().once('value').then(function (snapshot) {
-      return snapshot.val()
-    }).then((snapshot) => {
-      state = snapshot
-      renderUserList(state, sidebar)
+  console.log('contacting firebase...')
+  firebase.database().ref().once('value').then(function (snapshot) {
+    return snapshot.val()
+  }).then((snapshot) => {
+    // state = snapshot
+    // renderUserList(state, sidebar)
+    Object.keys(snapshot).forEach(function(key) {
+      console.log(key, snapshot[key])
+      state.users.push(snapshot[key])
     })
-      .catch((err) => {
-        console.error('there was a problem initialising the database', err)})
-  })()
-
-  delegate('body', 'click', '.mdl-button', function (event) {
-    console.log('you clicked', event.delegateTarget)
   })
+    .catch((err) => {
+      console.error('there was a problem initialising the database', err)})
+})()
 
-  // Render the user list
-  function renderUserList (data, into) {
-    console.log('rendering userList')
-    console.log(data.users)
-    into.innerHTML = `
+function grabInput () {
+  name.first
+  name.last
+  phone
+  about
+  address
+  age
+  comapny
+  email
+  favouriteFruit
+}
+
+delegate('body', 'click', '.mdl-button', function (event) {
+  console.log('you clicked', event.delegateTarget)
+})
+
+/*
+██████  ███████ ███    ██ ██████  ███████ ██████
+██   ██ ██      ████   ██ ██   ██ ██      ██   ██
+██████  █████   ██ ██  ██ ██   ██ █████   ██████
+██   ██ ██      ██  ██ ██ ██   ██ ██      ██   ██
+██   ██ ███████ ██   ████ ██████  ███████ ██   ██
+*/
+
+// Render the user list
+function renderUserList (data, into) {
+  console.log('rendering userList')
+  console.log(data.users)
+  into.innerHTML = `
     ${data.users.map((item, index) => {
-      return `
+    return `
         <li class="mdl-list__item mdl-list__item--one-line" data->
         <span class="mdl-list__item-primary-content">
           <i class="material-icons mdl-list__item-avatar">person</i>
           <span>${item.name.first} ${item.name.last}</span>
         </li>
         `
-    }).join('')}
+  }).join('')}
     `
-    componentHandler.upgradeDom()
-    console.log('renderUserlist complete', data)
-  }
+  componentHandler.upgradeDom()
+  console.log('renderUserlist complete', data)
+}
 
-  render(state, container)
+function renderUserDetail (data, into) {
+  // TODO
+}
 
-  function render (data, into) {
-    // TODO
-  }
-})()
+
+
+render(state, container)
+
+function render (data, into) {
+  // TODO
+}
+// })()
